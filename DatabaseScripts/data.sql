140d43f00ea4a39ec4ed543eb0393ceff0f770df
Danish <danishbhala573@gmail.com>
2021-05-15 18:13:56 +1200

    Add project files.

---
+++ DatabaseScripts/data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Event] ON
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (1, N'Open L8 : Raw Comedy Slam', N'2021-05-22 10:30:00', N'2021-05-22 17:00:00')
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (2, N'Spookers for Kids - Taste of Terror', N'2021-05-22 11:00:00', N'2021-05-22 16:00:00')
+INSERT INTO [dbo].[Event] ([Id], [EventName], [StartDate], [EndDate]) VALUES (3, N'Saturday Night Stand Up Comedy ', N'2021-05-22 20:00:00', N'2021-05-22 24:00:00')
+SET IDENTITY_INSERT [dbo].[Event] OFF
